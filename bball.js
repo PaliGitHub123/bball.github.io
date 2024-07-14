a00f9ccaf8b1d876492d848a9e8f9547f5b9eabf
PaliGitHub123 <146630889+PaliGitHub123@users.noreply.github.com>
2024-07-14 11:29:38 +0200

    Update bball.js

--- bball.js
+++ bball.js
@@ -1,57 +1,87 @@
-        const button = document.getElementById("add");
-        const body = document.getElementById("body");
 
-        function add(){
-            let img = document.createElement("img");
-            img.src = "Circle_(transparent).svg";
-            body.appendChild(img);
+function add(){
+    let img = document.createElement("img");
+    img.src = "Circle_(transparent).svg";
+    img.draggable = false; // Not using HTML5 draggable
+    body.appendChild(img);
 
-            var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-            img.onmousedown = dragMouseDown;
-            img.ontouchstart = dragMouseDown; // Added for mobile
+    img.onmousedown = dragMouseDown;
 
-            function dragMouseDown(e) {
-                e = e || window.event;
-                e.preventDefault();
-                if (e.type === 'touchstart') {
-                    pos3 = e.touches[0].clientX;
-                    pos4 = e.touches[0].clientY;
-                    document.ontouchend = closeDragElement;
-                    document.ontouchmove = elementDrag;
-                } else {
-                    pos3 = e.clientX;
-                    pos4 = e.clientY;
-                    document.onmouseup = closeDragElement;
-                    document.onmousemove = elementDrag;
-                }
-            }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // Call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
 
-            function elementDrag(e) {
-                e = e || window.event;
-                e.preventDefault();
-                if (e.type === 'touchmove') {
-                    pos1 = pos3 - e.touches[0].clientX;
-                    pos2 = pos4 - e.touches[0].clientY;
-                    pos3 = e.touches[0].clientX;
-                    pos4 = e.touches[0].clientY;
-                } else {
-                    pos1 = pos3 - e.clientX;
-                    pos2 = pos4 - e.clientY;
-                    pos3 = e.clientX;
-                    pos4 = e.clientY;
-                }
-                img.style.top = (img.offsetTop - pos2) + "px";
-                img.style.left = (img.offsetLeft - pos1) + "px";
-            }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Set the element's new position:
+        img.style.top = (img.offsetTop - pos2) + "px";
+        img.style.left = (img.offsetLeft - pos1) + "px";
+    }
 
-            function closeDragElement() {
-                if (event.type === 'touchend') {
-                    document.ontouchend = null;
-                    document.ontouchmove = null;
-                } else {
-                    document.onmouseup = null;
-                    document.onmousemove = null;
-                }
-            }
-        }
+    function closeDragElement() {
+        // Stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function add1(){
+    let img = document.createElement("img");
+    img.src = "Circle_(transparent).svg";
+    img.className = "gegenSpieler"
+    img.draggable = false; // Not using HTML5 draggable
+    body.appendChild(img);
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    img.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // Call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Set the element's new position:
+        img.style.top = (img.offsetTop - pos2) + "px";
+        img.style.left = (img.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // Stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+function loschen(){
+    body.removeChild(img);
+}
